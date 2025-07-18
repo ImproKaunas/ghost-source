ce33b6d648b101e879142c27055824e8dff0a324
Chionsas <Chionsas@users.noreply.github.com>
2025-07-18 14:50:03 +0300

    Remove unneeded languages. Leave LT and EN only.

--- ghost/i18n/lib/i18n.js
+++ ghost/i18n/lib/i18n.js
@@ -1,67 +1,67 @@
 const i18next = require('i18next');
 
 const SUPPORTED_LOCALES = [
-    'af', // Afrikaans
-    'ar', // Arabic
-    'bg', // Bulgarian
-    'bn', // Bengali
-    'bs', // Bosnian
-    'ca', // Catalan
-    'cs', // Czech
-    'da', // Danish
-    'de', // German
-    'de-CH', // Swiss German
-    'el', // Greek
+    // 'af', // Afrikaans
+    // 'ar', // Arabic
+    // 'bg', // Bulgarian
+    // 'bn', // Bengali
+    // 'bs', // Bosnian
+    // 'ca', // Catalan
+    // 'cs', // Czech
+    // 'da', // Danish
+    // 'de', // German
+    // 'de-CH', // Swiss German
+    // 'el', // Greek
     'en', // English
-    'eo', // Esperanto
-    'es', // Spanish
-    'et', // Estonian
-    'fa', // Persian/Farsi
-    'fi', // Finnish
-    'fr', // French
-    'gd', // Gaelic (Scottish)
-    'he', // Hebrew
-    'hi', // Hindi
-    'hr', // Croatian
-    'hu', // Hungarian
-    'id', // Indonesian
-    'is', // Icelandic
-    'it', // Italian
-    'ja', // Japanese
-    'ko', // Korean
-    'kz', // Kazach
+    // 'eo', // Esperanto
+    // 'es', // Spanish
+    // 'et', // Estonian
+    // 'fa', // Persian/Farsi
+    // 'fi', // Finnish
+    // 'fr', // French
+    // 'gd', // Gaelic (Scottish)
+    // 'he', // Hebrew
+    // 'hi', // Hindi
+    // 'hr', // Croatian
+    // 'hu', // Hungarian
+    // 'id', // Indonesian
+    // 'is', // Icelandic
+    // 'it', // Italian
+    // 'ja', // Japanese
+    // 'ko', // Korean
+    // 'kz', // Kazach
     'lt', // Lithuanian
-    'lv', // Latvian
-    'mk', // Macedonian
-    'mn', // Mongolian
-    'ms', // Malay
-    'nb', // Norwegian Bokmål
-    'ne', // Nepali
-    'nl', // Dutch
-    'nn', // Norwegian Nynorsk
-    'pa', // Punjabi (Gurmukhi, India)
-    'pl', // Polish
-    'pt', // Portuguese
-    'pt-BR', // Portuguese (Brazil)
-    'ro', // Romanian
-    'ru', // Russian
-    'si', // Sinhala
-    'sk', // Slovak
-    'sl', // Slovenian
-    'sq', // Albanian
-    'sr', // Serbian
-    'sr-Cyrl', // Serbian (Cyrillic)
-    'sv', // Swedish
-    'th', // Thai
-    'tr', // Turkish
-    'uk', // Ukrainian
-    'ur', // Urdu
-    'uz', // Uzbek
-    'vi', // Vietnamese
-    'zh', // Chinese
-    'zh-Hant', // Traditional Chinese
-    'sw', // Swahili
-    'ta' // Tamil
+    // 'lv', // Latvian
+    // 'mk', // Macedonian
+    // 'mn', // Mongolian
+    // 'ms', // Malay
+    // 'nb', // Norwegian Bokmål
+    // 'ne', // Nepali
+    // 'nl', // Dutch
+    // 'nn', // Norwegian Nynorsk
+    // 'pa', // Punjabi (Gurmukhi, India)
+    // 'pl', // Polish
+    // 'pt', // Portuguese
+    // 'pt-BR', // Portuguese (Brazil)
+    // 'ro', // Romanian
+    // 'ru', // Russian
+    // 'si', // Sinhala
+    // 'sk', // Slovak
+    // 'sl', // Slovenian
+    // 'sq', // Albanian
+    // 'sr', // Serbian
+    // 'sr-Cyrl', // Serbian (Cyrillic)
+    // 'sv', // Swedish
+    // 'th', // Thai
+    // 'tr', // Turkish
+    // 'uk', // Ukrainian
+    // 'ur', // Urdu
+    // 'uz', // Uzbek
+    // 'vi', // Vietnamese
+    // 'zh', // Chinese
+    // 'zh-Hant', // Traditional Chinese
+    // 'sw', // Swahili
+    // 'ta' // Tamil
 ];
 
 function generateResources(locales, ns) {
